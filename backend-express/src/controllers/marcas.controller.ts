c89a7d46cbbe19838f5e1f05c26f5ad6ca480764
Omar Sanchez Cuevas <OmR_Work@hotmail.com>
2024-05-17 12:21:46 -0400

    Marcas controller half completed

--- backend-express/src/controllers/marcas.controller.ts
+++ backend-express/src/controllers/marcas.controller.ts
@@ -1,26 +1,70 @@
 import { Request, Response } from "express";
+import { getAllMarcas, getOneMarca, insertMarca, removeMarca, updateMarca } from "../services/marcasService";
 
-export const getMarcas = (_req: Request, res: Response) => {
+export const getMarcas = async (_req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const response = await getAllMarcas();
+    res.status(200).json({
+      status: "OK",
+      data: { marcas: response },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const getMarca = (req: Request, res: Response) => {
+export const getMarca = async(req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const id = parseInt(req.params.id)
+    const response = await getOneMarca(id);
+    res.status(200).json({
+      status: "OK",
+      data: { marca: response },
+    });
+  } catch (error) {
+    console.log(error);
+    
+  }
 };
 
-export const postMarca = (req: Request, res: Response) => {
+export const postMarca = async(req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const response = await insertMarca(req.body);
+    res.status(200).json({
+      status: "OK",
+      data: { marca: response },
+    });
+  } catch (error) {
+    console.log(error);
+    
+  }
 };
 
-export const putMarca = (req: Request, res: Response) => {
+export const putMarca = async(req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    console.log(req.params.id);
+    
+    const id = parseInt(req.params.id)
+    const response = await updateMarca(req.body,id);
+    res.status(200).json({
+      status: "OK",
+      data: { marca: response },
+    });
+  } catch (error) {
+    console.log(error);
+    
+  }
 };
 
-export const deleteMarca = (req: Request, res: Response) => {
+export const deleteMarca = async(req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const id = parseInt(req.params.id)
+    const response = await removeMarca(id);
+    res.status(200).json({
+      status: "OK",
+      data: { marca: response },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
